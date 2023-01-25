ed94f20ba13fd445f21d15f92a3f1c6a0129f56d
leeseongbok <lsb@j-sol.co.kr>
2023-01-25 19:09:24 +0900

    [UPDATE] inputStream 읽기 테스트

--- socket/src/main/kotlin/kr/co/jsol/socket/InGSystemSerializerDeserializer.kt
+++ socket/src/main/kotlin/kr/co/jsol/socket/InGSystemSerializerDeserializer.kt
@@ -47,17 +47,9 @@ class InGSystemSerializerDeserializer : Serializer<String>, Deserializer<String>
     override fun deserialize(inputStream: InputStream): String {
         log.info("TCP InGSystem 역직렬화 작업 시작")
         try {
-//            val writer: BufferedWriter = BufferedWriter(System.out.writer(), 100)
-//            inputStream.bufferedReader().copyTo(writer)
-//            log.info("writer info : ${writer}")
-//            writer.flush()
-            val message = inputStream.readAllBytes().toString(charset)
-//            val message = writer.
-//            val message = parseString(inputStream)
+            val message = parseString(inputStream)
+//            val message = inputStream.readAllBytes().toString(charset)
             log.info("message = $message")
-
-//            log.info("is closed : ${inputStream.}")
-
             if (message.isNullOrBlank()) {
                 throw IOException("payload is null")
             }
@@ -66,6 +58,7 @@ class InGSystemSerializerDeserializer : Serializer<String>, Deserializer<String>
         } catch (e: SocketException) {
             log.error(e.stackTraceToString())
             log.error("TCP 역직렬화 중 Socket Exception 발생 : ${e.message}")
+            log.error(inputStream.available().toString())
             throw e
         } catch (e: IOException) {
             log.error("TCP 역직렬화 중 IOException 발생 message : ${e.message}")
@@ -79,7 +72,8 @@ class InGSystemSerializerDeserializer : Serializer<String>, Deserializer<String>
             log.error("TCP 역직렬화 중 에러 발생 InGSystemSerializerDeserializer.deserialize() : ${e.message}")
             throw e
         } finally {
-//            log.info("TCP InGSystem 역직렬화 작업 종료")
+            log.info("TCP InGSystem 역직렬화 작업 종료")
+//            if(inputStream.close())
 //            inputStream.close()
         }
     }
@@ -99,14 +93,14 @@ class InGSystemSerializerDeserializer : Serializer<String>, Deserializer<String>
 
             // -1 은 EOF임. 항상 마지막에 EOF을 나타내고 있음. 이 값이 읽히기 전까지는 데이터 input stream에 값이 남아있게 되므로
             // 10 12 13과 같은 값은 continue로 계속 읽도록 처리하고 -1만 break로 종료 함.
-            if (isEOF(c)) {
-                break
-            }
+//            if (isEOF(c)) {
+//                break
+//            }
 
             // 10 = LF 13 = CR 인데, 이 프로그램에서 10 13이 넘어오면 새로운 데이터가 들어올 예정이므로 종료한다.
-            if (c == 10 || c == 12 || c == 13) {
-                continue
-            }
+//            if (c == 10 || c == 12 || c == 13) {
+//                continue
+//            }
 
             builder.append(c.toChar())
         }
