318bc03fd7a7833418ab52bdd824c318c548fa7c
jax <ljsung0805@gmail.com>
2023-03-28 18:17:46 +0900

    [REFACTOR] authService 분리

--- api/src/main/kotlin/kr/co/jsol/api/controller/AuthController.kt
+++ api/src/main/kotlin/kr/co/jsol/api/controller/AuthController.kt
@@ -4,23 +4,19 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
-import io.swagger.v3.oas.annotations.security.SecurityRequirement
-import kr.co.jsol.domain.entity.user.User
-import kr.co.jsol.domain.entity.user.UserService
-import kr.co.jsol.domain.entity.user.dto.request.LoginRequest
-import kr.co.jsol.domain.entity.user.dto.request.UserRequest
-import kr.co.jsol.domain.entity.user.dto.response.LoginResponse
-import kr.co.jsol.domain.entity.user.dto.response.UserResponse
 import kr.co.jsol.common.jwt.dto.RefreshTokenDto
+import kr.co.jsol.domain.entity.user.AuthService
+import kr.co.jsol.domain.entity.user.User
+import kr.co.jsol.domain.entity.user.dto.request.LoginRequest
+import kr.co.jsol.domain.entity.user.dto.response.LoginResponse
 import org.springframework.http.HttpStatus
-import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/auth")
 class AuthController(
-    private val userService: UserService,
+    private val authService: AuthService
 ) {
 
     @Operation(summary = "토큰 재발급")
@@ -32,7 +28,7 @@ class AuthController(
     @ResponseStatus(value = HttpStatus.OK)
     // PreAuthorize를 사용하지 않으려면 security config 에서 정의한다.
     fun refreshToken(@AuthenticationPrincipal userDetails: User): RefreshTokenDto {
-        return userService.refreshToken(userDetails.username)
+        return authService.refreshToken(userDetails.username)
     }
 
     @Operation(summary = "로그인")
@@ -43,6 +39,6 @@ class AuthController(
     @PostMapping("/login")
     @ResponseStatus(value = HttpStatus.OK)
     fun login(@RequestBody loginRequest: LoginRequest): LoginResponse {
-        return userService.login(loginRequest)
+        return authService.login(loginRequest)
     }
 }
--- domain/src/main/kotlin/kr/co/jsol/domain/entity/user/UserService.kt
+++ domain/src/main/kotlin/kr/co/jsol/domain/entity/user/UserService.kt
@@ -2,31 +2,21 @@ package kr.co.jsol.domain.entity.user
 
 import kr.co.jsol.common.exception.entities.user.UserAlreadyExistException
 import kr.co.jsol.common.exception.entities.user.UserDisableException
-import kr.co.jsol.common.jwt.JwtTokenProvider
-import kr.co.jsol.common.jwt.dto.RefreshTokenDto
 import kr.co.jsol.domain.entity.site.Site
 import kr.co.jsol.domain.entity.site.SiteRepository
 import kr.co.jsol.domain.entity.site.dto.response.SiteResponse
-import kr.co.jsol.domain.entity.user.dto.request.LoginRequest
 import kr.co.jsol.domain.entity.user.dto.request.UserRequest
 import kr.co.jsol.domain.entity.user.dto.request.UserUpdateRequest
-import kr.co.jsol.domain.entity.user.dto.response.LoginResponse
 import kr.co.jsol.domain.entity.user.dto.response.UserResponse
 import kr.co.jsol.domain.entity.user.enums.UserRoleType
 import kr.co.jsol.domain.entity.util.findByIdOrThrow
 import org.slf4j.LoggerFactory
 import org.springframework.data.repository.findByIdOrNull
-import org.springframework.http.HttpStatus
-import org.springframework.security.authentication.*
-import org.springframework.security.core.AuthenticationException
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
-import org.springframework.web.server.ResponseStatusException
 
 @Service
 class UserService(
-    private val jwtTokenProvider: JwtTokenProvider,
-    private val authenticationManager: AuthenticationManager,
     private val userRepository: UserRepository,
     private val userQuerydslRepository: UserQuerydslRepository,
     private val siteRepository: SiteRepository,
@@ -35,49 +25,8 @@ class UserService(
 
     private val log = LoggerFactory.getLogger(UserService::class.java)
 
-    fun refreshToken(userId: String): RefreshTokenDto {
-        return RefreshTokenDto(jwtTokenProvider.createRefreshToken(userId))
-    }
-
-    fun login(loginRequest: LoginRequest): LoginResponse {
-        val user: User = userRepository.findByIdAndLockedIsFalse(loginRequest.username)
-            ?: throw ResponseStatusException(HttpStatus.FORBIDDEN, "로그인 실패하셨습니다.")
-
-        try {
-            log.info("username : ${user.username}\npassword : ${loginRequest.password}")
-            authenticationManager.authenticate(
-                UsernamePasswordAuthenticationToken(
-                    user.username,
-                    loginRequest.password
-                )
-            )
-        } catch (e: AuthenticationException) {
-            e.printStackTrace()
-            throw ResponseStatusException(HttpStatus.FORBIDDEN, "로그인 실패하셨습니다.")
-        } catch (e: LockedException) {
-            throw ResponseStatusException(HttpStatus.FORBIDDEN, "계정이 잠겨 있습니다.")
-        } catch (e: DisabledException) {
-            throw ResponseStatusException(HttpStatus.FORBIDDEN, "계정이 비활성화 상태입니다.")
-        } catch (e: CredentialsExpiredException) {
-            throw ResponseStatusException(HttpStatus.FORBIDDEN, "비밀번호가 만료 되었습니다.")
-        } catch (e: AccountExpiredException) {
-            throw ResponseStatusException(HttpStatus.FORBIDDEN, "계정이 만료되었습니다.")
-        }
-
-        return LoginResponse(
-            role = user.role,
-            siteSeq = user.site!!.id,
-            accessToken = jwtTokenProvider.createAccessToken(user.username),
-            refreshToken = jwtTokenProvider.createRefreshToken(user.username),
-        )
-    }
-
     fun isExistUserById(id: String): Boolean {
         return userRepository.existsById(id)
-    }
-
-    fun getById(id: String): User {
-        return userRepository.findByIdOrThrow(id, "계정 정보를 찾을 수 없습니다.")
     }
 
     fun getAll(): List<UserResponse> {
@@ -127,7 +76,7 @@ class UserService(
             site = site,
         )
 
-        if(password.isNotBlank()){
+        if (password.isNotBlank()) {
             val newPassword = passwordEncoder.encode(password)
             user.updatePassword(newPassword)
         }
