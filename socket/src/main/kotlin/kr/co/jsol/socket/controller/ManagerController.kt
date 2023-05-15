85ed6b1f1b12a8a8a29ec943aa4c8d3dc11ee7fc
leeseongbok <lsb@j-sol.co.kr>
2023-05-15 17:49:50 +0900

    [FEAT] Socket rest path 수정

--- socket/src/main/kotlin/kr/co/jsol/socket/controller/ManagerController.kt
+++ socket/src/main/kotlin/kr/co/jsol/socket/controller/ManagerController.kt
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/manage")
+@RequestMapping("/socket/manage")
 @Schema(
     description = "서버 관리 API",
 )
