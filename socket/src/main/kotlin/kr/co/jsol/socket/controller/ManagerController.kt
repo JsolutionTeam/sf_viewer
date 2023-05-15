3391c6afeff374487543b19e3ae6f39c2a6840b0
leeseongbok <lsb@j-sol.co.kr>
2023-05-15 17:46:58 +0900

    [REFACTOR] ktlint 포매팅

--- socket/src/main/kotlin/kr/co/jsol/socket/controller/ManagerController.kt
+++ socket/src/main/kotlin/kr/co/jsol/socket/controller/ManagerController.kt
@@ -4,10 +4,8 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
-import kr.co.jsol.domain.entity.user.dto.request.UserRequest
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.ResponseStatus
