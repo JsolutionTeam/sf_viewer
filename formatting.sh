82b8381a2979deccadc2a42ac69d7858dad13bf7
leeseongbok <lsb@j-sol.co.kr>
2023-06-07 13:23:38 +0900

    [FEAT] lint 체크용 추가

---
+++ formatting.sh
@@ -0,0 +1,3 @@
+./gradlew :common:ktlintFormat :domain:ktlintFormat :api:ktlintFormat :socket:ktlintFormat :rda:ktlintFormat --stacktrace
+
+./gradlew :common:ktlintCheck :domain:ktlintCheck :api:ktlintCheck :socket:ktlintCheck :rda:ktlintCheck --stacktrace
